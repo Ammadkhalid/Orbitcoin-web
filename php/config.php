25dbc0881d62a0ddc463d7fd967d12ce15acbb9d
Ammadkhalid <Ammadkhalid12@gmail.com>
2015-12-22 21:13:51 +0500

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,5 +1,7 @@
 <?php
 require_once "jsonRPCClient.php";
+// config database i.e the localhost is host, root is username, and the empty filed is password, orbitcoin is database
 $con = mysqli_connect("localhost", "root", "", "orbitcoin");
+/// here put ur orbitcoin wallet with user pass and port
 $coin = new jsonRPCClient("http://Orb:Orbitcoin_password@127.0.0.1:15299");
 ?>
