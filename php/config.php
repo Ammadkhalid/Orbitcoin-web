022bb7dcc58f73c2f512b866344af46cc5c8705f
Ammadkhalid <Ammadkhalid12@gmail.com>
2015-12-22 21:15:08 +0500

    Edit this file for running web-wallet

--- php/config.php
+++ php/config.php
@@ -2,6 +2,7 @@
 require_once "jsonRPCClient.php";
 // config database i.e the localhost is host, root is username, and the empty filed is password, orbitcoin is database
 $con = mysqli_connect("localhost", "root", "", "orbitcoin");
-/// here put ur orbitcoin wallet with user pass and port
+// here put ur orbitcoin wallet with user pass and port
+// this project is incomplete 
 $coin = new jsonRPCClient("http://Orb:Orbitcoin_password@127.0.0.1:15299");
 ?>
